84b715ed5faa6479a43a5e5566576bb69d37717f
Kevin Yue <yuezk001@gmail.com>
2022-08-14 19:57:43 +0800

    fix: improve the condition check for the started status

--- src/main/java/com/poratu/idea/plugins/tomcat/conf/ServerConsoleView.java
+++ src/main/java/com/poratu/idea/plugins/tomcat/conf/ServerConsoleView.java
@@ -22,15 +22,24 @@ public class ServerConsoleView extends ConsoleViewImpl implements ConsoleView {
     @Override
     public void print(@NotNull String s, @NotNull ConsoleViewContentType contentType) {
         super.print(s, contentType);
-        if (!printStarted) {
-            if (s.contains("org.apache.catalina.startup.Catalina start")
-             || s.contains("org.apache.catalina.startup.Catalina.start")) {
-                String url = "http://localhost" + (configuration.getPort().equals("80") ? "" : ":" + configuration.getPort()) + configuration.getContextPath();
-                super.print(url + "\n", contentType);
-                printStarted = true;
-            }
+
+        if (printStarted) {
+            return;
+        }
+
+        // skip the exception log e.g.:
+        // at org.apache.catalina.startup.Catalina.start(Catalina.java:772)
+        boolean isExceptionLog = s.trim().startsWith("at ");
+        if (isExceptionLog) {
+            return;
+        }
+
+        if (s.contains("org.apache.catalina.startup.Catalina start")
+                || s.contains("org.apache.catalina.startup.Catalina.start")) {
+            String url = "http://localhost" + (configuration.getPort().equals("80") ? "" : ":" + configuration.getPort()) + configuration.getContextPath();
+            super.print(url + "\n", contentType);
+            printStarted = true;
         }
     }
-
 
 }
