afc668af0e95f1519183f0b584d420da1a451a2b
Kevin Yue <yuezk001@gmail.com>
2022-08-21 12:33:53 +0800

    fix: do not allow override catalina.home

--- src/main/java/com/poratu/idea/plugins/tomcat/conf/TomcatCommandLineState.java
+++ src/main/java/com/poratu/idea/plugins/tomcat/conf/TomcatCommandLineState.java
@@ -142,7 +142,7 @@ public class TomcatCommandLineState extends JavaCommandLineState {
 
             ParametersList vmParams = javaParams.getVMParametersList();
             vmParams.addParametersString(vmOptions);
-            vmParams.defineProperty(PARAM_CATALINA_HOME, tomcatInstallationPath.toString());
+            vmParams.addProperty(PARAM_CATALINA_HOME, tomcatInstallationPath.toString());
             vmParams.defineProperty(PARAM_CATALINA_BASE, workingPath.toString());
             vmParams.defineProperty(PARAM_CATALINA_TMPDIR, workingPath.resolve("temp").toString());
             vmParams.defineProperty(PARAM_LOGGING_CONFIG, confPath.resolve("logging.properties").toString());
